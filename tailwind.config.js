a8d47e8c120d0fa17c87d81419d010ea60a06cd6
David Daza <03dazal@gmail.com>
2022-03-14 14:00:59 -0500

    Initial landing render

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,33 @@
 module.exports = {
-  content: [
-    "./layouts/**/*.html"
-  ],
+  purge: {
+    enabled: process.env.HUGO_ENVIRONMENT === 'production',
+    content: [
+      './layouts/**/*.html'
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      colors: {
+        'blue-green': '#1B5C4F',
+        'big-dip-oruby': '#9F2241',
+        camel: '#BC955C',
+        'eerie-black': '#141414',
+        'cultured': '#fefefe',
+        nickel: '#6F7271',
+        'tyrian-purple': '#5F1234'
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        'source-sans-pro': ['"Source Sans Pro"', 'sans-serif'],
+        gotham: ['Gotham', 'sans-serif']
+      }
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/aspect-ratio')
+  ],
 }
